b674a9642e6836f5c1efc67798c1cbfb14640fd8
u300639 <u300639@VPN2SUS-004210.hubfintech.local>
2021-10-26 13:57:38 -0300

    save

--- primeiro-programa-java/src/curso/java/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/curso/java/executavel/PrimeiraClasseJava.java
@@ -3,7 +3,9 @@ package curso.java.executavel;
 public class PrimeiraClasseJava {
 	
 	public static void main(String[] args) {
-		System.out.println("Meu primeiro programa em JAVA!");
-	}
+		System.out.println("Meu primeiro programa em JAVA!!!!!!");
+	
+	 System.out.println("Meu segundo Projeto em JAVA");
 
+}
 }
